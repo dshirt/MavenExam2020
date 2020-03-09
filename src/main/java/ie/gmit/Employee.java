6ffb7c6ccd0a914045ff8e3c6055f468efdf92aa
JOHN LAWLESS - STUDENT <G00351835@gmit.ie>
2020-03-09 10:22:41 +0000

    This is my working code test 100% complete

--- src/main/java/ie/gmit/Employee.java
+++ src/main/java/ie/gmit/Employee.java
@@ -4,6 +4,12 @@
 * This is an Employee Class created for a maven test
 * it must contain:
 * Title, name, pps number, phone, employment type and age
+* Title must be a valid title Mr, Mrs, Ms
+* Name has length restrictions
+* PPS number must be 7 characters long
+* Phone Number must be 6 characters long
+* Employment Type can only be Full-Time or Part-Time
+* Age must be over 18 to be valid
 * */
 
 
@@ -24,7 +30,7 @@ public class Employee {
             this.title = title;
         else
             throw new IllegalArgumentException("Title not valid");
-        if((name.length()<5) && (name.length()>22))
+        if((name.length()<5) || (name.length()>22))
             throw new IllegalArgumentException("Your name must contain 5 to 22 characters");
         else
             this.name = name;
@@ -35,7 +41,7 @@ public class Employee {
         if(phoneNumber.length()== 7)
             this.phoneNumber = phoneNumber;
         else
-            throw new IllegalArgumentException("PPS not valid");
+            throw new IllegalArgumentException("Phone number not valid");
         if (employmentType.equalsIgnoreCase("Full-time") ||
                 employmentType.equalsIgnoreCase("Part-time"))
             this.employmentType = employmentType;
@@ -51,26 +57,5 @@ public class Employee {
     public String getTitle() {
         return title;
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPpsNumber() {
-        return ppsNumber;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public String getEmploymentType() {
-        return employmentType;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
 
 }
--- src/test/java/ie/gmit/TestEmployee.java
+++ src/test/java/ie/gmit/TestEmployee.java
@@ -1,8 +1,10 @@
 /*
  * John Lawless
  * G00351835@gmit.ie
- * This is an Employee  Test Class created for a maven test
- *It Test the Employee Class and a jacoco report is created at the end of the test
+ * This is an Employee  Test Class created for a maven in class exam
+ *It Tests the Employee Class and a jacoco report is created at the end of the test
+ * The initial test, tests all parameters of the Employee class as valid inputs
+ * The Mr title is also checked as valid in this initial test.
  * */
 
 package ie.gmit;
@@ -11,19 +13,24 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 public class TestEmployee {
 
     Employee employee;
 
-    @DisplayName("***************Testing the creation of an Employee object title Mr")
+    /*All parameters are tested as valid in this initial test so no exceptions
+    * are thrown, also using this so specifically test the title parameter for a Mr input*/
+    @DisplayName("***************Testing the creation of an Employee object with all valid parameters testing for title Mr" )
     @Test
     void testConstructorEmployeeTitleMr(){
         employee = new Employee("Mr", "John Lawless", "12345T", "0860498", "Full-Time", 49);
         assertEquals("Mr", employee.getTitle());
     }
 
+
+    /*Testing the Mrs title input, alll other parameters also valid*/
     @DisplayName("***************Testing the creation of an Employee object title Mrs")
     @Test
     void testConstructorEmployeeTitleMrs(){
@@ -37,6 +44,77 @@ public class TestEmployee {
         employee = new Employee("Ms", "Eilidh Lawless", "12345T", "0860498", "Part-Time", 49);
         assertEquals("Ms", employee.getTitle());
     }
+    //Test invalid title input
+    @DisplayName("***************Testing the creation of an Employee object  Invalid title ")
+    @Test
+    void testConstructorEmployeeInvalidTitle(){
+        final String invalid = "Title not valid";
+        Exception title = assertThrows(IllegalArgumentException.class,()->new Employee("M", "Eilidh Lawless",
+                "12345T", "0860498", "Part-Time", 49));
+        assertEquals(invalid, title.getMessage());
+    }
+
+    //test name too short
+    @DisplayName("***************Testing the creation of an Employee object name length too short ")
+    @Test
+    void testConstructorEmployeeInvalidNameLengthToShort(){
+        final String invalid = "Your name must contain 5 to 22 characters";
+        Exception name = assertThrows(IllegalArgumentException.class,()->new Employee("Ms", "Eil",
+                "12345T", "0860498", "Part-Time", 49));
+        assertEquals(invalid, name.getMessage());
+    }
+
+    //test name too long
+    @DisplayName("***************Testing the creation of an Employee object name length too long ")
+    @Test
+    void testConstructorEmployeeInvalidNameLengthToLong(){
+        final String invalid = "Your name must contain 5 to 22 characters";
+        Exception name = assertThrows(IllegalArgumentException.class,()->new Employee("Ms", "abcdefghijklmnopqrstuvwxyz",
+                "12345T", "0860498", "Part-Time", 49));
+        assertEquals(invalid, name.getMessage());
+    }
+
+
+    //test incorrect pps length
+    @DisplayName("***************Testing the creation of an Employee object PPs incorrect length ")
+    @Test
+    void testConstructorEmployeePPSIncorrectLength(){
+        final String invalid = "PPS not valid";
+        Exception message = assertThrows(IllegalArgumentException.class,()->new Employee("Ms", "Eilidh Lawless",
+                "12345", "0860498", "Part-Time", 49));
+        assertEquals(invalid, message.getMessage());
+    }
+
+    //test incorrect Phone number length
+    @DisplayName("***************Testing the creation of an Employee object Phone number incorrect length ")
+    @Test
+    void testConstructorEmployeePhoneNumberLength(){
+        final String invalid = "Phone number not valid";
+        Exception message = assertThrows(IllegalArgumentException.class,()->new Employee("Ms", "Eilidh Lawless",
+                "12345T", "086049", "Part-Time", 49));
+        assertEquals(invalid, message.getMessage());
+    }
+
+    //test incorrect Employement type
+    @DisplayName("***************Testing the creation of an Employee object incorrect Employment type ")
+    @Test
+    void testConstructorEmployeeEmploymentType(){
+        final String invalid = "Employment Type not valid";
+        Exception message = assertThrows(IllegalArgumentException.class,()->new Employee("Ms", "Eilidh Lawless",
+                "12345T", "0860492", "No-Time", 49));
+        assertEquals(invalid, message.getMessage());
+    }
+
+    //test under age
+    @DisplayName("***************Testing the creation of an Employee object under age ")
+    @Test
+    void testConstructorEmployeeIncorrectAge(){
+        final String invalid = "You are under 18";
+        Exception message = assertThrows(IllegalArgumentException.class,()->new Employee("Ms", "Eilidh Lawless",
+                "12345T", "0860492", "Full-Time", 16));
+        assertEquals(invalid, message.getMessage());
+    }
+
 
 
 
